7e5632b531c12e703c9ca70d0cdd27d882bb33e5
yuvraj <yuvrajahuja4@gmail.com>
2024-09-04 18:21:06 +0530

    fixed sigin page navigation

--- client/src/Pages/SigninPage.jsx
+++ client/src/Pages/SigninPage.jsx
@@ -3,18 +3,21 @@ import { Description } from "../components/Description.jsx";
 import { InputField } from "../components/InputField.jsx";
 import { Button } from "../components/Button.jsx";
 import { BottomLogin } from "../components/BottomLogin.jsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { Navbar } from "../components/Navbar.jsx";
 
 export function SigninPage() {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   const navigate = useNavigate();
   return (
-    <div className=" h-screen bg-slate-300 flex justify-center">
-      <div className=" flex flex-col justify-center">
-        <div className="rounded-lg w-80 h-max text-center p-2  pb-10 px-4 bg-white">
+    <div className=" h-screen bg-slate-300">
+      <Navbar />
+      <div className=" flex flex-col justify-center items-center mt-32 ">
+        <div className="rounded-lg w-80 h-max text-center p-2 px-4 bg-white">
           <Heading label="Sign In" />
           <Description info="Enter registered username and password" />
           <InputField
@@ -31,15 +34,24 @@ export function SigninPage() {
             <Button
               label="Sign In"
               onClick={async () => {
-                const response = await axios.post(
-                  "https://pay-pro-api.vercel.app/api/v1/user/signin",
-                  {
-                    username,
-                    password,
-                  },
-                );
-                localStorage.setItem("token", response.data.token);
-                navigate("/dashboard");
+                try {
+                  const response = await axios.post(
+                    "https://pay-pro-api.vercel.app/api/v1/user/signin",
+                    {
+                      username,
+                      password,
+                    },
+                  );
+
+                  if (response && response.status === 200) {
+                    localStorage.setItem("token", response.data.token);
+                    navigate("/dashboard");
+                  } else {
+                    console.log("Sign-in failed:");
+                  }
+                } catch (err) {
+                  console.log(err);
+                }
               }}
             />
           </div>
